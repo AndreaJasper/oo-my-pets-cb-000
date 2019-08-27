503ee23d5546524e868c303a00765b33beb19ae9
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-08-27 14:07:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,42 +8,53 @@ class Owner
    @species = 'human'
    @pets = {:dogs => [], :cats => []}
    @@all << self
+   @@count += 1
 end
+
 def name
   @name.dup.freeze
 end
+
  def self.all
    @@all
  end
+
  def self.reset_all
    @@all = []
  end
+
  def self.count
    @@all.length
  end
+
  def say_species
      "I am a human."
  end
+
  def buy_cat(name)
    cat = Cat.new(name)
    self.pets[:cats] << cat
    cat
  end
+
  def buy_dog(name)
    dog = Dog.new(name)
    self.pets[:dogs] << dog
    dog
  end
+
  def walk_dogs
    self.pets[:dogs].each do|dog|
    dog.mood = "happy"
    end
  end
+
  def play_with_cats
    self.pets[:cats].each do|cat|
    cat.mood = "happy"
    end
  end
+
  def sell_pets
    self.pets.each do| types,pets|
      pets.each do|pet|
@@ -52,6 +63,7 @@ end
    end
    @pets = {:dogs => [], :cats => []}
  end
+
  def list_pets
    return "I have #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
  end
